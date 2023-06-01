74c354e276bc5b54f8f4295904bd9b1613b57979
Tamefuzball7 <sebastianduquereyes@gmail.com>
2023-06-01 18:18:56 -0500

    Update settings.py

--- social_project/settings.py
+++ social_project/settings.py
@@ -51,11 +51,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -164,8 +164,9 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = ['*']
 
+
+CSRF_TRUSTED_ORIGINS = ['mangakualpha-production.up.railway.app']
 
 
 
