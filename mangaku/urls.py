b63599c4b178a88073bf359f3096e7d81ef6f803
Tamefuzball7 <sebastianduquereyes@gmail.com>
2023-06-01 18:52:02 -0500

    update

--- mangaku/urls.py
+++ mangaku/urls.py
@@ -39,5 +39,4 @@ urlpatterns = [
     path('change_password/', views.change_password, name='change_password'),
     
     
-
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social_project/urls.py
+++ social_project/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mangaku.urls')),
     
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
