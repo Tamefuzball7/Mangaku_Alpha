9b9fd7f7037ca71ab6ed2888baf0f4667ba6457a
Tamefuzball7 <sebastianduquereyes@gmail.com>
2023-06-01 18:07:02 -0500

    Update settings.py

--- social_project/settings.py
+++ social_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import psycopg2
 import os
+from corsheaders.middleware import CorsMiddleware
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
