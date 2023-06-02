7fc24b05be4b01d68b891cee1a696f1714cc8f2a
Tamefuzball7 <sebastianduquereyes@gmail.com>
2023-06-02 12:58:21 -0500

    Deploy docker

--- social_project/settings.py
+++ social_project/settings.py
@@ -134,8 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    'static',
+    os.path.join(BASE_DIR, 'static')
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
