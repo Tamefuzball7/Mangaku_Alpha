1ad6408d36efddfea4648a11840d73ff9a8372e9
Tamefuzball7 <sebastianduquereyes@gmail.com>
2023-06-03 03:24:54 -0500

    Update settings.py

--- social_project/settings.py
+++ social_project/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-   ,
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
